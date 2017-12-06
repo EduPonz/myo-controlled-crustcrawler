8fd718a3c9ca4276e8794c51faaea74cc2a1ea61
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2017-12-06 19:58:55 +0100

    Update system_controller.ino
    Adding the functionality of receiving and parsing a JSON message and some minor corrections

--- arduino-to-crustcrawler-interface/system_controller.ino
+++ arduino-to-crustcrawler-interface/system_controller.ino
@@ -6,33 +6,54 @@
 
 const int k_p = 400;
 const int k_v = 40;
+const float ALLOWED_ERROR = 2; // Some value
 const int GRIPPER_DELAY = 3000;
+const String JOB_FAILED = "failed";
+const String JOB_SUCCESS = "success";
+const String JOB_IN_PROGRESS = "in progress";
+
 DynamicsCalculator dynamics_calculator;
 pathPlanning path_planning;
 DynamixelPro2 dynamixel;
 UnitsConverter convert;
+
 float servo_position[3] = {0, 0, 0};
 float servo_velocity[3] = {0, 0, 0};
 float servo_acceleration[3] = {0, 0, 0};
 float desired_position[3] = {0, 0, 0};
 float desired_velocity[3] = {0, 0, 0};
 float desired_acceleration[3] = {0, 0, 0};
-int mode;
-bool path_status = false;
-String job_status = "failed";
+
 int operation_id = 1;
-String prev_instruction = "";
+String job_status = JOB_FAILED;
+int mode = 0;
 String instruction = "";
+bool path_status = false;
+String gesture = dynamixel.UNKNOWN_GESTURE;
+
+String prev_instruction = "";
+unsigned long prev_read_millis = millis();
 
 void setup(){
 	Serial.begin(9600);
 }
 
 void loop(){
+
+	while (!Serial) {}
+
+	if (Serial.available() > 0)	{
+		if (millis() - prev_read_millis > 100) {
+			String input = "";
+			input = serialInput();
+			applyInstructions(input);
+		}
+	}
+
 	if (mode == dynamixel.PRE_SET_MODE){
 		instruction = dynamixel.get_instruction(dynamixel.PRE_SET_MODE, gesture);
 		if (instruction == dynamixel.EXTENDED || instruction == dynamixel.TO_USER || instruction == dynamixel.HOME){
-			job_status = "in progress";
+			job_status = JOB_IN_PROGRESS;
 			get_servo_positions();
 			path_status = path_planning.calculate_path(instruction, servo_position[0], servo_position[1], servo_position[2]);
 			send_json();
@@ -45,8 +66,10 @@ void loop(){
 					float servo_acc [3];
 					if (time >= sampling_rate || time == 0){
 						for (int i = 0; i < 3; ++i){
-							servo_error_pos[i] = convert.position_degrees_to_radians(convert.unit_to_degree(dynamixel.get_current_position(i)) - path_planning.get_positionSample(i, time)) * k_p;
-							servo_error_vel[i] = convert.position_degrees_to_radians(convert.unit_to_degree(dynamixel.get_current_velocity(i)) - path_planning.get_velocitySample(i, time)) * k_v;
+							servo_error_pos[i] = convert.position_degrees_to_radians(convert.unit_to_degree(dynamixel.get_current_position(i)) 
+											   - path_planning.get_positionSample(i, time)) * k_p;
+							servo_error_vel[i] = convert.position_degrees_to_radians(convert.unit_to_degree(dynamixel.get_current_velocity(i)) 
+											   - path_planning.get_velocitySample(i, time)) * k_v;
 							servo_acc[i] = path_planning.get_accelerationSample(i, time);
 						}
 						dynamics_calculator.set_thetas(servo_error_pos[0], servo_error_pos[1], servo_error_pos[2]);
@@ -56,13 +79,13 @@ void loop(){
 						dynamixel.write_torque(torque, time_for_torque);
 						prev_time = millis();
 					}
-					if (abs(path_planning.get_goal_position(0, instruction) - convert.unit_to_degree(dynamixel.get_current_position(0))) < allowed_error){
+					if (abs(path_planning.get_goal_position(0, instruction) - convert.unit_to_degree(dynamixel.get_current_position(0))) < ALLOWED_ERROR){
 						first_ok = true;
 					}
-					if (abs(path_planning.get_goal_position(1, instruction) - convert.unit_to_degree(dynamixel.get_current_position(1))) < allowed_error){
+					if (abs(path_planning.get_goal_position(1, instruction) - convert.unit_to_degree(dynamixel.get_current_position(1))) < ALLOWED_ERROR){
 						second_ok = true;
 					}
-					if (abs(path_planning.get_goal_position(2, instruction) - convert.unit_to_degree(dynamixel.get_current_position(2))) < allowed_error){
+					if (abs(path_planning.get_goal_position(2, instruction) - convert.unit_to_degree(dynamixel.get_current_position(2))) < ALLOWED_ERROR){
 						third_ok = true;
 					}
 					if (first_ok && second_ok && third_ok){
@@ -71,31 +94,34 @@ void loop(){
 					}
 				}
 				get_servo_positions();
-				job_status = "success";
+				job_status = JOB_SUCCESS;
 				send_json()
-				job_status = "failed";
+				job_status = JOB_FAILED;
 				path_status = false;
 				first_ok = false;
 				second_ok = false;
 				third_ok = false;
-				operation_id++;
 			}else{
 				get_servo_positions();
-				job_status = "failed";
-				operation_id++;
+				job_status = JOB_FAILED;
 			}
+
 		}else if (instruction == dynamixel.GRIPPER){
 			dynamixel.use_gripper();
 			delay(GRIPPER_DELAY);
-			job_status = "success";
+			job_status = JOB_SUCCESS;
 			get_servo_positions();
 			send_json();
-			operation_id++;
+		}else{
+			get_servo_positions();
+			job_status = JOB_FAILED;
 		}
+		operation_id++;
+		mode = 0;
 
 	}else if (mode == dynamixel.MANUAL_MODE){
 		instruction = dynamixel.get_instruction(dynamixel.MANUAL_MODE, gesture);
-		job_status = "success";
+		job_status = JOB_SUCCESS;
 
 		if (instruction == dynamixel.DOWN){
 			dynamixel.move_down();
@@ -106,7 +132,7 @@ void loop(){
 		}else if (instruction == dynamixel.RIGHT){
 			dynamixel.move_right();
 		}else{
-			job_status = "failed";
+			job_status = JOB_FAILED;
 		}
 
 		if (prev_instruction != instruction){
@@ -115,6 +141,7 @@ void loop(){
 			prev_instruction = instruction;
 			operation_id++;
 		}
+		mode = 0;
 	}
 }
 
@@ -124,16 +151,32 @@ void get_servo_positions(){
 	}
 }
 
+String serialInput() {
+	int n = Serial.available();
+	String s = Serial.readString();
+	return s;
+}
+
+void applyInstructions(String instructions) {
+	StaticJsonBuffer<200> jsonBuffer;
+	JsonObject& jsonInstructions = jsonBuffer.parseObject(instructions);
+	mode = jsonInstructions["mode"].toInt();
+	gesture = jsonInstructions["gesture"];
+}
+
 void send_json(){
 	StaticJsonBuffer<200> jsonBuffer;
     JsonObject& jsonMessage = jsonBuffer.createObject();
-    jsonMessage["operation id"] = operation_id;
-    jsonMessage["job status"] = job_status;
-    jsonMessage["instruction"] = instruction;
-    jsonMessage["path status"] = path_status;
-    jsonMessage["servo 1 position"] = servo_position[0];
-    jsonMessage["servo 2 position"] = servo_position[1];
-    jsonMessage["servo 3 position"] = servo_position[2];
+    jsonMessage["operation id"] = operation_id; 		 // int
+    jsonMessage["mode"] = mode;							 // int
+    jsonMessage["gesture"] = gesture;					 // String
+    jsonMessage["job status"] = job_status;				 // String
+    jsonMessage["instruction"] = instruction;			 // String
+    jsonMessage["path status"] = path_status;			 // bool
+    jsonMessage["servo 1 position"] = servo_position[0]; // float
+    jsonMessage["servo 2 position"] = servo_position[1]; // float
+    jsonMessage["servo 3 position"] = servo_position[2]; // float
     jsonMessage.printTo(Serial);
     Serial.println();
+    Serial.flush();
 }
