4a44260994e658ef2ca7d7ae97ba6e6a63b5e133
Svendsen92 <ssven16@student.aau.dk>
2017-12-06 10:41:49 +0100

    Add files via upload

---
+++ arduino-to-crustcrawler-interface/libraries/Dynamixel_Pro/Dynamixel_Demo.ino
@@ -0,0 +1,51 @@
+/*
+ Name:		Dynamixel_Demo.ino
+ Created:	12/5/2017 3:25:09 PM
+ Author:	Steffan Svendsen, Vincent Joly, Simone Jensen, David Michalik, Eduardo Ponz Segrelles, Ivelin Krasimirov Penchev 
+*/
+
+#include "DynamixelPro2.h"
+
+#define Baudrate 57600
+
+DynamixelPro2 dynamixel;
+
+void setup() {
+
+	Serial.begin(115200);
+	dynamixel.begin(Baudrate);
+	dynamixel.write_holding_torque();
+	delay(3000);
+	Serial.println("");
+	Serial.println("Ready");
+	Serial.println("");
+
+}
+
+
+void loop() {
+  
+}
+
+void read_demo() {
+
+	for (int i = 0; i < 5; i++) {
+		Serial.print("Servo ");
+		Serial.print(i+1);
+		Serial.print(" current position = ");
+		Serial.println(dynamixel.read_current_position(i + 1));
+
+		Serial.println("");
+		Serial.print("Servo ");
+		Serial.print(i + 1);
+		Serial.print(" current velocity = ");
+		Serial.println(dynamixel.read_current_velocity(i + 1));
+	}
+}
+
+void write_demo() {
+
+	int servo_id = 1;
+	unsigned int position = 90;
+	dynamixel.write_goal_position(servo_id, position);
+}
