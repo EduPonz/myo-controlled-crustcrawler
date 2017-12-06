ab41151e30814f3f41d0c4c28f5602b3e77d0aa4
Eduardo Ponz Segrelles <e.ponzs@gmail.com>
2017-12-06 10:38:04 +0100

    Add files via upload

---
+++ arduino-to-crustcrawler-interface/libraries/Dynamixel_Pro/DynamixelPro2.cpp
@@ -0,0 +1,328 @@
+/*
+Name:		Dynamixel_Demo.cpp
+Created:	12/5/2017 3:25:09 PM
+Author:		Steffan Svendsen, Vincent Joly, Simone Jensen, David Michalik, Eduardo Ponz Segrelles, Ivelin Krasimirov Penchev
+*/
+
+#include "DynamixelPro2.h"
+
+DynamixelPro2::DynamixelPro2(){}
+
+
+void DynamixelPro2::begin(long baud) {
+
+#if defined(__AVR_ATmega32U4__) || defined(__MK20DX128__) || defined(__AVR_ATmega2560__)
+	Serial1.begin(baud);  // Set up Serial 
+	this->_serial = &Serial1;
+#else
+	Serial.begin(baud);   // Set up Serial for all others (Uno, etc)
+	this->_serial = &Serial;
+#endif
+
+}
+
+
+void DynamixelPro2::end() {
+
+#if defined(__AVR_ATmega32U4__) || defined(__MK20DX128__) || defined(__AVR_ATmega2560__)
+	Serial1.end();
+#else
+	Serial.end();
+#endif
+
+}
+
+
+void DynamixelPro2::move_left() {
+	int Pos1 = this->read_current_position(0x01);
+	Pos1 = Pos1 + 20;
+	this->write_goal_position(0x01, Pos1);
+
+}
+
+
+void DynamixelPro2::move_right() {
+	int Pos2 = this->read_current_position(0x01);
+	Pos2 = Pos2 - 20;
+	this->write_goal_position(0x01, Pos2);
+}
+
+
+void DynamixelPro2::move_up() {
+	int Pos3 = this->read_current_position(0x02);
+	if (Pos3 <= 2022) {
+		Pos3 = Pos3 + 90;
+		this->write_goal_position(0x02, Pos3);
+	}else {
+		Pos3 = Pos3 - 90;
+		this->write_goal_position(0x02, Pos3);
+	}
+}
+
+
+void DynamixelPro2::move_down() {
+	int Pos3 = this->read_current_position(0x02);
+	if (Pos3 <= 2022) {
+		Pos3 = Pos3 - 90;
+		this->write_goal_position(0x02, Pos3);
+	}else {
+		Pos3 = Pos3 + 90;
+		this->write_goal_position(0x02, Pos3);
+	}
+}
+
+
+void DynamixelPro2::write_holding_torque() {
+
+	for (int i = 1; i < 6; i++) {		
+
+		unsigned char arr[1] = {'Set'};
+		this->_write_to_servo_id(i, 0x40, arr, 1);
+	}
+}
+
+
+void DynamixelPro2::write_torque(int servo_id, float torque, float time_of_torque) {
+
+
+}			
+
+
+void DynamixelPro2::_write_to_servo_id(int servo_id, unsigned short addr, unsigned char *arr, int n) {
+
+	n += 5;													// This skips the first 5 bits
+	unsigned char ID = this->_servo_HEX_id[servo_id - 1];
+
+	this->_instruction_packet_array[0] = servo_id;
+	this->_instruction_packet_array[1] = (n & 0xFF);				//length
+	this->_instruction_packet_array[2] = (n & 0xFF00) >> 8;		//length
+	this->_instruction_packet_array[3] = 0x03;					//Instruction
+	this->_instruction_packet_array[4] = (addr & 0xFF);			//address
+	this->_instruction_packet_array[5] = (addr & 0xFF00) >> 8;	//address
+
+	for (int i = 0; i < n - 5; i++) {
+		this->_instruction_packet_array[i + 6] = arr[i];
+	}
+
+	this->_clear_RX_buffer();
+	this->_transmit_instruction_packet(n);
+}
+
+
+void DynamixelPro2::write_goal_position(int servo_id, unsigned int pos) {
+
+	pos %= 4096;
+	unsigned char ID = this->_servo_HEX_id[servo_id - 1];
+
+	unsigned char arr[] = {
+		(pos & 0xFF),
+		(pos & 0xFF00) >> 8,
+		(pos & 0xFF0000) >> 16,
+		(pos & 0xFF000000) >> 24
+	};
+
+	this->_write_to_servo_id(ID, 0x74, arr, 4);
+}
+
+
+int DynamixelPro2::read_current_position(int servo_id) {
+
+	unsigned char ID = this->_servo_HEX_id[servo_id - 1];
+
+	this->_clear_RX_buffer();
+	this->_read_from_servo_id(ID, 0x84, 4);			//Read from adress 0x84 (Present Position), byte size 4
+	this->_read_parameters();							//Filters parameters from ReturnPacket
+
+	int sum = (_data[2] << 8) | _data[1];       //Converting two information bytes into a integer (position data)
+	return (sum);
+}
+
+
+int DynamixelPro2::read_current_velocity(int servo_id) {
+
+	unsigned char ID = this->_servo_HEX_id[servo_id - 1];
+
+	this->_clear_RX_buffer();
+	this->_read_from_servo_id(ID, 0x80, 4);				//Read from adress 0x80 (Present Velocity), byte size 4
+	this->_read_parameters();							//Filters parameters from ReturnPacket
+
+	int sum = (_data[2] << 8) | _data[1];				//Converting two information bytes into a integer (position data)
+	return (sum);
+}
+
+
+void DynamixelPro2::_read_from_servo_id(unsigned char ID, unsigned short addr, int n) {
+
+	n += 3;
+	this->_instruction_packet_array[0] = ID;
+	this->_instruction_packet_array[1] = (n & 0xFF);					//length of packet
+	this->_instruction_packet_array[2] = (n & 0xFF00) >> 8;			//length of packet
+	this->_instruction_packet_array[3] = 0x02;						//Instruction
+	this->_instruction_packet_array[4] = (addr & 0xFF);				//address
+	this->_instruction_packet_array[5] = (addr & 0xFF00) >> 8;		//address
+	this->_instruction_packet_array[6] = ((n - 3) & 0xFF);			//data length
+	this->_instruction_packet_array[7] = ((n - 3) & 0xFF00) >> 8;		// data length
+
+	this->_clear_RX_buffer();
+	this->_transmit_instruction_packet(n);
+	this->_read_return_packet();
+}
+
+
+void DynamixelPro2::_read_parameters(void) {
+
+	int j = 0;
+	for (int i = 0; i < 100; i++) {
+		
+		if (this->_return_packet[i] == 0x55 && this->_return_packet[i - 1] == 0 && this->_return_packet[i + 1] == 0) {	//Filtering the parameters from the returnpacket, by searching for the instruction (0x00, 0x55, 0x00)
+			
+			this->_data[j] = this->_return_packet[i - 3];							//Saving ID
+
+			this->_data[j + 1] = this->_return_packet[i + 2];						//Saving parameter bytes
+			this->_data[j + 2] = this->_return_packet[i + 3];						//Saving parameter bytes
+
+			j += 3;
+		}
+	}
+
+}
+
+
+void DynamixelPro2::_read_return_packet(void) {
+
+	int i = 0;
+	while (this->_serial->available()>0) {					//Read information when available
+		int incomingbyte = this->_serial->read();			//Save incomingbyte
+
+		this->_return_packet[i] = incomingbyte;				//Save data in ReturnPacket array
+		i++;
+	}
+}
+
+
+void DynamixelPro2::_clear_RX_buffer(void) {
+
+	while (this->_serial->read() != -1);			// Clears the RX buffer;
+
+}
+
+
+void DynamixelPro2::_transmit_instruction_packet(int transLen) {
+
+	// Transmit instruction packet to Dynamixel
+	if (this->_direction_pin > -1) {
+		digitalWrite(this->_direction_pin, HIGH);                       // Set TX Buffer pin to HIGH
+	}
+
+	int i;
+	int arrLen = transLen + 7;
+
+	unsigned char* pt;
+	pt = new unsigned char[7 + transLen];
+
+	//unsigned char arrLen = transLen + 7;
+	//unsigned char pt[arrLen];
+
+	pt[0] = 0xFF;
+	pt[1] = 0xFF;
+	pt[2] = 0xFD;
+	pt[3] = 0x00;
+
+
+	int i;
+	for (int i = 0; i <= transLen; i++) {
+		pt[i + 4] = _instruction_packet_array[i];
+	}
+
+	unsigned short crc = this->_update_crc(pt, arrLen - 2);
+	unsigned char CRC_L = (crc & 0x00FF);
+	unsigned char CRC_H = (crc >> 8) & 0x00FF;
+
+	i += 4;
+
+	pt[i++] = CRC_L;
+	pt[i] = CRC_H;
+
+	for (int i = 0; i < arrLen; i++) {
+		this->_serial->write(pt[i]);
+	}
+
+	noInterrupts();
+
+#if defined(__AVR_ATmega32U4__) || defined(__MK20DX128__) || defined(__AVR_ATmega2560__) // Leonardo and Mega use Serial1
+	if ((UCSR1A & B01100000) != B01100000) {                                             // Wait for TX data to be sent
+		_serial->flush();
+	}
+
+#elif defined(__SAM3X8E__)
+
+	//if(USART_GetFlagStatus(USART1, USART_FLAG_TC) != RESET)
+	this->_serial->flush();
+	//}
+
+#else
+	if ((UCSR0A & B01100000) != B01100000) {                                             // Wait for TX data to be sent
+		this->_serial->flush();
+	}
+
+#endif
+
+	if (this->_direction_pin > -1) {
+		digitalWrite(this->_direction_pin, LOW);                                                //Set TX Buffer pin to LOW after data has been sent
+	}
+
+	interrupts();
+
+	delay(20);
+
+}
+
+
+unsigned short DynamixelPro2::_update_crc(unsigned char *data_blk_ptr, unsigned short data_blk_size)
+{
+	unsigned short crc_accum = 0;
+	unsigned short i, j;
+	unsigned short crc_table[256] = {
+		0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011,
+		0x8033, 0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022,
+		0x8063, 0x0066, 0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072,
+		0x0050, 0x8055, 0x805F, 0x005A, 0x804B, 0x004E, 0x0044, 0x8041,
+		0x80C3, 0x00C6, 0x00CC, 0x80C9, 0x00D8, 0x80DD, 0x80D7, 0x00D2,
+		0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB, 0x00EE, 0x00E4, 0x80E1,
+		0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE, 0x00B4, 0x80B1,
+		0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087, 0x0082,
+		0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,
+		0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1,
+		0x01E0, 0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1,
+		0x81D3, 0x01D6, 0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2,
+		0x0140, 0x8145, 0x814F, 0x014A, 0x815B, 0x015E, 0x0154, 0x8151,
+		0x8173, 0x0176, 0x017C, 0x8179, 0x0168, 0x816D, 0x8167, 0x0162,
+		0x8123, 0x0126, 0x012C, 0x8129, 0x0138, 0x813D, 0x8137, 0x0132,
+		0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E, 0x0104, 0x8101,
+		0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317, 0x0312,
+		0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,
+		0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371,
+		0x8353, 0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342,
+		0x03C0, 0x83C5, 0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1,
+		0x83F3, 0x03F6, 0x03FC, 0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2,
+		0x83A3, 0x03A6, 0x03AC, 0x83A9, 0x03B8, 0x83BD, 0x83B7, 0x03B2,
+		0x0390, 0x8395, 0x839F, 0x039A, 0x838B, 0x038E, 0x0384, 0x8381,
+		0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E, 0x0294, 0x8291,
+		0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7, 0x02A2,
+		0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,
+		0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1,
+		0x8243, 0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252,
+		0x0270, 0x8275, 0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261,
+		0x0220, 0x8225, 0x822F, 0x022A, 0x823B, 0x023E, 0x0234, 0x8231,
+		0x8213, 0x0216, 0x021C, 0x8219, 0x0208, 0x820D, 0x8207, 0x0202
+	};
+
+	for (j = 0; j < data_blk_size; j++)
+	{
+		i = ((unsigned short)(crc_accum >> 8) ^ data_blk_ptr[j]) & 0xFF;
+		crc_accum = (crc_accum << 8) ^ crc_table[i];
+	}
+
+	return crc_accum;
+}
+
---
+++ arduino-to-crustcrawler-interface/libraries/Dynamixel_Pro/DynamixelPro2.h
@@ -0,0 +1,52 @@
+/*
+Name:		Dynamixel_Demo.h
+Created:	12/5/2017 3:25:09 PM
+Author:		Steffan Svendsen, Vincent Joly, Simone Jensen, David Michalik, Eduardo Ponz Segrelles, Ivelin Krasimirov Penchev
+*/
+
+#ifndef DynamixelPro2_h
+#define DynamixelPro2_h
+
+#include "Arduino.h"
+
+#define NONE                            0x00
+#define READ                            0x01
+#define ALL                             0x02
+
+class DynamixelPro2 {
+				public:
+						DynamixelPro2() : _direction_pin(-1), _status_return_value(READ) { }
+						void begin(long);
+						void end(void);
+						void move_left();
+						void move_right();
+						void move_up();
+						void move_down();
+						void write_holding_torque();
+						void write_torque(int servo_id, float torque, float time_of_torque);
+						void write_goal_position(int servo_id, unsigned int pos);
+						int read_current_position(int servo_id);
+						int read_current_velocity(int servo_id);
+
+				private:
+						Stream *_serial;
+
+						void _write_to_servo_id(int servo_id, unsigned short addr, unsigned char *arr, int n);
+						void _read_from_servo_id(unsigned char ID, unsigned short addr, int n);
+						void _read_return_packet(void);
+						void _read_parameters(void);
+						void _clear_RX_buffer(void);
+						void _transmit_instruction_packet(int transLen);
+						unsigned short _update_crc(unsigned char *data_blk_ptr, unsigned short data_blk_size);
+						
+
+						char _direction_pin;									// Pin to control TX/RX buffer chip
+						unsigned int _data[15];									// Data from ReturnPacket
+						unsigned int _return_packet[100];						// Array to hold returned status packet data
+						unsigned char _instruction_packet_array[64];			// Array to hold instruction packet data
+						unsigned char _status_return_value;						// Status packet return states ( NON , READ , ALL )	
+						unsigned char _servo_HEX_id[5] = {0x01, 0x02, 0x03, 0x04, 0x05};	// Array to hold servo id's in HEX form
+
+};
+
+#endif
