4be15bd46bce860a90ddce7d6f9712048d81a705
Svendsen92 <ssven16@student.aau.dk>
2017-12-11 19:42:26 +0100

    Added torque_to_PWM_unit

--- arduino-to-crustcrawler-interface/libraries/converter/UnitsConverter.cpp
+++ arduino-to-crustcrawler-interface/libraries/converter/UnitsConverter.cpp
@@ -7,13 +7,31 @@
 
 #include "Arduino.h"
 #include "UnitsConverter.h"
+#include "DynamixelPro2.h"
 
 int UnitsConverter::degree_to_unit(float degree) {
 	return(degree / this->_UNIT_TO_DEGREE);
 }
 
-int UnitsConverter::torque_to_unit(float torque) {
-	return(1); //To Do
+int UnitsConverter::torque_to_PWM_unit(int servo_id, float torque) {	// This function needs to be looked at to see if the logic is correct.
+
+	float B, k, R;
+
+	float vel = unit_to_degree(dynamixel.read_current_velocity(servo_id));
+
+	if (servo_id == 0) {		// For MX-64 servo.		Check that the servo_id correspond with the correct servo.
+		B = 2.3 / 33;
+		k = 2.4 / 2.1;
+		R = 18.2;
+	}else {						// For MX 106 servo.
+		B = 2.6 / 18; 
+		k = 3.3 / 2.1;
+		R = 29.3;
+	}
+
+	int goalPWM = (((((torque + B*vel) / k)*R) + k*vel) / 12.5) * 885;
+
+	return(goalPWM);
 }
 
 float UnitsConverter::unit_to_degree(int unit) {
--- arduino-to-crustcrawler-interface/libraries/converter/UnitsConverter.h
+++ arduino-to-crustcrawler-interface/libraries/converter/UnitsConverter.h
@@ -1,7 +1,7 @@
 /*
  Name:		UnitsConverter.h
  Created:	12/1/2017 11:42:29 AM
- Author:	Steffan Svendsen, Vincent Joly, Simone Jensen, David Michalik, Eduardo Ponz Segrelles, Ivelin Krasimirov Penchev
+ Author:	Steffan Svendsen, Vincent Joly, Simone Jensen, David Michalik, Eduardo Ponz Segrelles, Ivelin Krasimirov Penchev, Jesper Bro Kirstein Rosenberg
  Editor:	http://www.visualmicro.com
 */
 
@@ -9,25 +9,29 @@
 #define UnitsConverter_h
 
 #include "Arduino.h"
+#include "DynamixelPro2.h"
 
 class UnitsConverter
 {
 public:
-		int degree_to_unit(float degree);
-		int torque_to_unit(float torque);
-		float unit_to_degree(int unit);
-		float unit_to_radians(int unit);
-		float position_degrees_to_radians(float theta);
-		float speed_degrees_to_radians(float omega);
-		float acceleration_degrees_to_radians(float alpha);
+	int degree_to_unit(float degree);
+	int torque_to_PWM_unit(int servo_id, float torque);
+	float unit_to_degree(int unit);
+	float unit_to_radians(int unit);
+	float position_degrees_to_radians(float theta);
+	float speed_degrees_to_radians(float omega);
+	float acceleration_degrees_to_radians(float alpha);
 
 private:
-		const float _PI = 3.1415927;
-		const float _UNIT_TO_DEGREE = 0.088;
-		const float _UNIT_TO_RAD = _UNIT_TO_DEGREE * _PI / 180;
-		const float _DEGREE_TO_RAD = _PI / 180;
-		const float _SPEED_RAD_TO_DEGREE = 57.29578;
-		const float _ACC_DEGREE_TO_RAD = 0.0174533;
+
+	DynamixelPro2 dynamixel;
+
+	const float _PI = 3.1415927;
+	const float _UNIT_TO_DEGREE = 0.088;
+	const float _UNIT_TO_RAD = _UNIT_TO_DEGREE * _PI / 180;
+	const float _DEGREE_TO_RAD = _PI / 180;
+	const float _SPEED_RAD_TO_DEGREE = 57.29578;
+	const float _ACC_DEGREE_TO_RAD = 0.0174533;
 };
 
 #endif
